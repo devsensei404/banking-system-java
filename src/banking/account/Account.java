729d2475b327f7e728fec62e10eacb505f6d533d
devsensei404 <subhadeeppaul51@gmail.com>
2026-07-12 18:13:07 +0000

    draft

--- src/banking/account/Account.java
+++ src/banking/account/Account.java
@@ -1,4 +1,36 @@
 package banking.account;
 
 public class Account {
+
+    private static long counter=11111111;
+    private long accountNumber;
+    private String holderName;
+    private double balance;
+
+    Account(String name) {
+        this.accountNumber=counter++;
+        this.holderName=name;
+        this.balance=0.0;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountNumber=" + accountNumber +
+                ", holderName='" + holderName + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
 }
